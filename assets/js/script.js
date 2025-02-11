fcce55e88a8b0b9ee692c4e1df127c0356436d65
BRAGGZ9 <brianbinomugisha9@gmail.com>
2025-02-11 16:14:11 +0300

    back to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
 
+// Show or hide button based on scroll position
+window.onscroll = function () {
+    let button = document.getElementById("backToTop");
+    if (document.documentElement.scrollTop > 200) {
+        button.classList.add("show");
+    } else {
+        button.classList.remove("show");
+    }
+};
+
+// Smooth scroll to top function
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
