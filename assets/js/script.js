001d4b44692c7d75f7747eb8e0da74fc8c15e396
BRAGGZ9 <brianbinomugisha9@gmail.com>
2025-02-12 14:01:20 +0300

    joinning js files into one

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,4 +88,22 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+// autoscrolling to different section
+document.addEventListener("DOMContentLoaded", function () {
+    // Select all menu links
+    const menuLinks = document.querySelectorAll(".menu a");
+  
+    menuLinks.forEach(link => {
+      link.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent default link behavior
+        
+        const targetId = this.getAttribute("href").substring(1); // Get section ID
+        const targetSection = document.getElementById(targetId); // Find target section
+  
+        if (targetSection) {
+          targetSection.scrollIntoView({ behavior: "smooth" }); // Smooth scroll
+        }
+      });
+    });
+  });
   
